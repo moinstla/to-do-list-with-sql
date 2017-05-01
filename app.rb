ccec8c4efca323f4a20572d256dea47fe0c5f121
Steven Galvin and Sean Pierce <sean@>
2017-05-01 15:44:27 -0700

    finish implimenting user stories as integration specs

--- app.rb
+++ app.rb
@@ -1,13 +1,43 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require './lib/task'
+require './lib/list'
 require 'pry'
 require 'pg'
 
 also_reload('lib/**/*.rb')
 
-DB = PG.connect({:dbname => "to_do"})
+DB = PG.connect({:dbname => "to_do_test"})
 
 get('/') do
   erb(:index)
+end
+
+get("/lists/new") do
+  erb(:list_form)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save
+  erb(:success)
+end
+
+get("/lists") do
+  @lists = List.all
+  erb(:lists)
+end
+
+get("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i)
+  erb(:list)
+end
+
+post("/tasks") do
+  description = params.fetch("description")
+  list_id = params.fetch("list_id").to_i
+  task = Task.new({:description => description, :list_id => list_id})
+  task.save
+  erb(:success)
 end
--- lib/list.rb
+++ lib/list.rb
@@ -19,10 +19,31 @@ class List
 
   def save
     result = DB.exec("INSERT INTO lists (name) VALUES ('#{@name}') RETURNING id;")
-    @id = result.first().fetch("id").to_i()
+    @id = result.first.fetch("id").to_i
   end
 
   def ==(another_list)
     self.name.==(another_list.name).&(self.id.==(another_list.id))
   end
+
+  def List.find(id)
+    List.all.each do |list|
+      if list.id == id
+        return list
+      end
+    end
+  end
+
+  def tasks
+    list_tasks = []
+    tasks = DB.exec("SELECT * FROM tasks WHERE list_id = #{self.id};")
+    tasks.each do |task|
+      description = task.fetch('description')
+      list_id = task.fetch('list_id').to_i
+      id = task.fetch('id').to_i
+      list_tasks.push(Task.new({:description => description, :list_id => list_id, :id => id}))
+    end
+    list_tasks
+  end
+
 end
